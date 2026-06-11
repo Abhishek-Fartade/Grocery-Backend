70ea2a6bc306ba4971c9dc47a808ed36394db8bf
Abhishek Fartade <abhishekfartade41@gmail.com>
2026-06-11 11:18:54 +0530

    fix auth endpoints

--- src/main/java/com/supermarket/controller/UserController.java
+++ src/main/java/com/supermarket/controller/UserController.java
@@ -20,7 +20,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserController {
     
     @Autowired
