9faee76591c1ecd3b1316591ea3fa082306ad080
Abhishek Fartade <abhishekfartade41@gmail.com>
2026-06-11 10:46:43 +0530

    Fix CORS

--- src/main/java/com/supermarket/security/SecurityConfig.java
+++ src/main/java/com/supermarket/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // Allow both user frontend and admin frontend
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001","https://grocery-frontend-nu-eight.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
